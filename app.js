076dce43f597caffad41ef5338e6b1e4219d7500
Vladimir Bozhkov <vl.bozhkov@gmail.com>
2018-09-04 14:25:08 +0300

    finished the blog app

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var port = 3012;
+var methodOverride = require('method-override');
+var expressSanitizer = require('express-sanitizer');
+var port = 3001;
 
 //connect to database
 var uri = 'mongodb://vl_bozhkov:12345678@ds151702.mlab.com:51702/main_database';
@@ -17,6 +19,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // set up body parser
 app.use(bodyParser.urlencoded({ extended: true }));
+//express sanitizer setup
+app.use(expressSanitizer());
+//method-override
+app.use(methodOverride('_method'));
 
 //mongoose schema config
 var blogSchema = new mongoose.Schema({
@@ -48,6 +54,8 @@ app.get('/blogs/new', function(req, res) {
 
 //CREATE ROUTE
 app.post('/blogs', function(req, res) {
+  //sanitize so user cannot income harmful information like script tags
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(e, newBlog) {
     if (e) {
       res.render('new');
@@ -66,6 +74,41 @@ app.get('/blogs/:id', function(req, res) {
     }
   });
 });
+//EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res) {
+  Blog.findById(req.params.id, function(e, foundBlog) {
+    if (e) {
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', { blog: foundBlog });
+    }
+  });
+});
+//UPDATE ROUTE
+app.put('/blogs/:id', function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
+    e,
+    updatedBlog
+  ) {
+    if (e) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs/' + req.params.id);
+    }
+  });
+});
+//DELETE ROUTE
+app.delete('/blogs/:id', function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, function(e) {
+    if (e) {
+      res.redirect('/');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
 //set up server to run at port 3000
 app.listen(port, function() {
   console.log('server listen on port = ', port);
